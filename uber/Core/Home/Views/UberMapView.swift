93a60738bd27ecd7f3f4a16632968adb61edfb15
romanesssss <82055122+romanesssss@users.noreply.github.com>
2024-01-24 23:32:02 +0300

    commit 6
    - Add polyline
    But line doesn't remove after select a new location

--- uber/Core/Home/Views/UberMapView.swift
+++ uber/Core/Home/Views/UberMapView.swift
@@ -26,6 +26,7 @@ struct UberMapView: UIViewRepresentable {
     func updateUIView(_ uiView: UIViewType, context: Context) {
         if let coordinate = locationViewModel.selectedLocationCoordinate {
             context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
         }
     }
     
@@ -38,6 +39,7 @@ extension UberMapView {
     
     class MapCoordinator: NSObject, MKMapViewDelegate {
         let parent: UberMapView
+        var userLocationCoordinate: CLLocationCoordinate2D?
         
         init(parent: UberMapView) {
             self.parent = parent
@@ -45,11 +47,20 @@ extension UberMapView {
         }
         
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude,                                                                 longitude: userLocation.coordinate.latitude),
                                             span: MKCoordinateSpan(latitudeDelta: 0.05,
                                                                    longitudeDelta: 0.05))
             
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBlue
+            polyline.lineWidth = 6
+            return polyline
+            
         }
         
         func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
@@ -58,6 +69,37 @@ extension UberMapView {
             anno.coordinate = coordinate
             self.parent.mapView.addAnnotation(anno)
             self.parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+        
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            getDestinationRoute(from: userLocationCoordinate,
+                                to: coordinate) { route in
+                self.parent.mapView.addOverlay(route.polyline)
+            }
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                                 to destination: CLLocationCoordinate2D,
+                                 completion: @escaping(MKRoute) -> Void) {
+            let userPlacemark = MKPlacemark(coordinate: userLocation)
+            let destPlacemark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destPlacemark)
+            let direction = MKDirections(request: request)
+            
+            direction.calculate { response, error in
+                if let error = error {
+                    print("Debug: Failed to get directions with error \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let route = response?.routes.first else { return }
+                completion(route)
+            }
         }
     }
 }
